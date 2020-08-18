78367407b6da1263eb1f6522a03e6b4224d305f0
hyeyeon2964 <hyeyeon2964@hanmail.net>
2020-08-19 00:11:28 +0900

    baekjoon l3

---
+++ src/baekjoon/level3/별찍기1.java
@@ -0,0 +1,20 @@
+package baekjoon.level3;
+//첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+//첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+import java.io.*;
+public class 별찍기1 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        if(1<=n && n<=100){
+            }
+        }
+
+
+
+    }
+
+
+}
