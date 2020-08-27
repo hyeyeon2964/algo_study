6ddcfc4a674596466df532081c8815920b189522
hyeyeon2964 <hyeyeon2964@hanmail.net>
2020-08-27 17:52:57 +0900

    programmers l1

--- src/programmers/level1/완주하지못한선수.java
+++ src/programmers/level1/완주하지못한선수.java
@@ -1,10 +1,23 @@
 package programmers.level1;
 
+import java.util.Arrays;
+
 public class 완주하지못한선수 {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        Arrays.sort(participant);
+        Arrays.sort(completion);
 
-        for(int i = 0; i)
+        for(int i = 0; i<completion.length; i++){
+            if(!(participant[i].equals(completion[i]))) {
+                answer = participant[i];
+                break;
+            }
+        }
         return answer;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
