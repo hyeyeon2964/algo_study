9c54dd27c1f54e6c61ec400344cec15e3caea559
hyeyeon2964 <hyeyeon2964@hanmail.net>
2020-10-02 20:25:38 +0900

    level1

--- src/programmers/level1/비밀지도.java
+++ src/programmers/level1/비밀지도.java
@@ -2,9 +2,27 @@ package programmers.level1;
 
 public class 비밀지도 {
     public static String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
+        String[] answer = new String[n];
+        StringBuffer[] str = new StringBuffer[n];
+        int binary;
+        for(int i = 0; i<n; i++){
+            binary = (arr1[i] | arr2[i]); // 비트 연산으로 or계산
+            int index = 0;
+            str[i] = new StringBuffer(); //초기화 안해주면 nullpointer 에러 발생
 
+            while(index < n){ //str에 결과 값의 반대로 집어넣음
+                if(binary % 2 == 1)
+                    str[i].append('#');
+                else
+                    str[i].append(' ');
+                index++;
+                binary /= 2;
+            }
 
+        }
+        for(int i = 0; i<n; i++){
+            answer[i] = str[i].reverse().toString(); //반대로 만들어 answer에 넣음
+        }
         return answer;
     }
 
@@ -13,6 +31,9 @@ public class 비밀지도 {
         int[] arr1 = {9, 20, 28, 18, 11};
         int[] arr2 = {30, 1, 21, 17, 28};
 
-        System.out.println(solution(n, arr1, arr2));
+        String[] arr = solution(n, arr1, arr2);
+        for(String s : arr){
+            System.out.println(s);
+        }
     }
 }
---
+++ src/programmers/level1/실패율.java
@@ -0,0 +1,18 @@
+package programmers.level1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 실패율 {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        Map<Integer, Double> map = new HashMap<>();
+
+        for(int i = 0; i<stages.length; i++){
+            int key = stages[i];
+            double value;
+        }
+
+        return answer;
+    }
+}
