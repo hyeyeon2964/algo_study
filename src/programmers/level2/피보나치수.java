be604877d6efb742d65747f334933b5a203c2710
MyoungHyeYeon <hyeyeon2964@hanmail.net>
2020-11-12 16:08:28 +0900

    study

--- src/programmers/level2/피보나치수.java
+++ src/programmers/level2/피보나치수.java
@@ -10,11 +10,10 @@ public class 피보나치수 {
         list.add(0);
         list.add(1);
         for(int i = 2; i <= n; i++) {
-            list.add(list.get(i-1)+list.get(i-2));
+            list.add((list.get(i-1)+list.get(i-2)) % 1234567);
         }
 
         answer = list.get(n);
-        answer %= 1234567;
         return answer;
     }
 }
