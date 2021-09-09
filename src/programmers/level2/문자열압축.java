acfbdc59e50c5b62245e55f10233b1b8d1603daf
hyeyeon2964 <hyeyeon2964@hanmail.net>
2021-09-09 21:53:14 +0900

    카카오 문제 풀이

--- src/programmers/level2/문자열압축.java
+++ src/programmers/level2/문자열압축.java
@@ -1,10 +1,40 @@
 package programmers.level2;
 
 public class 문자열압축 {
-    public int solution(String s) {
+    public static int solution(String s) {
         int answer = 0;
+
+
+        StringBuffer sb = new StringBuffer();
+        int cnt = 1;
+        for (int i = 1; i <s.length()/2; i++) {
+            String temp = s.substring(0, i);
+            for (int j = i + 1; j <s.length(); j += i) {
+                if (temp.contains(s.substring(i, j))) {
+                    cnt++;
+                }
+                else {
+                    if (cnt > 1) {
+                        sb.append(Integer.toString(cnt));
+                        sb.append(temp);
+                        temp = s.substring(j,j+i);
+                        cnt = 1;
+                    }
+
+                }
+            }
+            if (cnt > 1) {
+                sb.append(Integer.toString(cnt));
+                sb.append(temp);
+            }
+            answer = Math.min(answer, sb.length());
+        }
 
         
         return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("aabbaccc"));
     }
 }
---
+++ src/programmers/level2/오픈채팅방2.java
@@ -0,0 +1,53 @@
+package programmers.level2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class 오픈채팅방2 {
+    public static String[] solution(String[] record) {
+        String[] answer;
+        ArrayList<String> chatInfo = new ArrayList<>();
+        ArrayList<HashMap<String, String>> nickName = new ArrayList<>();
+        String[] id = new String[record.length];
+        ArrayList<String> result = new ArrayList<>();
+
+        for (int i = 0; i < record.length; i++) {
+            String[] temp = record[i].split(" ");
+            chatInfo.add(temp[0]);
+            id[i] = temp[1];
+            HashMap<String, String> map = new HashMap();
+            map.put(temp[1], temp[2]);
+            nickName.add(map);
+        }
+
+        for (int i = 0; i < record.length; i++) {
+            String str = "";
+            if (chatInfo.contains("Change")) {
+                continue;
+            }
+            if (chatInfo.contains("Enter")) {
+                str += nickName.get(i).get(id[i]);
+                str += "님이 들어왔습니다.";
+            } else {
+                str += nickName.get(i).get(id[i]);
+                str += "님이 나갔습니다.";
+            }
+            result.add(str);
+        }
+
+        answer = new String[result.size()];
+
+        for (int i = 0; i< answer.length; i++) {
+            answer[i] = result.get(i);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] arr = new String
+        System.out.println(solution(new String[]));
+    }
+
+}
+
