455849040ae0ae4a0475a9f9214952de7826a376
MyoungHyeYeon <hyeyeon2964@hanmail.net>
2021-10-06 13:15:21 +0900

    다익스트라 알고리즘

--- src/baekjoon/binary_search/나무자르기.java
+++ src/baekjoon/binary_search/나무자르기.java
@@ -3,10 +3,11 @@ package baekjoon.binary_search;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class 나무자르기 {
-    static int[] tree;
+    static int[] trees;
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st;
@@ -14,14 +15,32 @@ public class 나무자르기 {
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        tree = new int[n];
+        trees = new int[n];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i<n; i++){
-            tree[i] = Integer.parseInt(st.nextToken());
+            trees[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(trees);
+        long end = trees[trees.length - 1];
+        long start = 0;
 
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            long sum = 0;
+            for (int tree : trees) {
+                if (tree > mid) {
+                    sum += tree - mid;
+                }
+            }
+            if (sum >= m) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
 
-
+        }
+        System.out.println(end);
     }
 }
---
+++ src/baekjoon/binary_search/랜선자르기.java
@@ -0,0 +1,15 @@
+package baekjoon.binary_search;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 랜선자르기 {
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+    }
+}
