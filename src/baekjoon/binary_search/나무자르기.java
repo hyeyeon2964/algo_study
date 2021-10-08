9ace86dcf24d29b7ccd63a79e8d1163377a16e4e
hyeyeon2964 <hyeyeon2964@hanmail.net>
2021-10-08 23:11:47 +0900

    알고리즘

--- src/baekjoon/binary_search/나무자르기.java
+++ src/baekjoon/binary_search/나무자르기.java
@@ -4,7 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/*
+ * [upper bound 방식]
+ * int[] tree : 입력받은 나무들의 높이가 저장되어있는 배열(정렬할 필요 없음)
+ * min : 하한선 변수로 초기값은 0
+ * max : tree 에 저장 된 나무들 중 가장 큰 나무의 높이
+ */
 public class 나무자르기 {
     static int[] tree;
 
@@ -12,16 +17,40 @@ public class 나무자르기 {
         StringTokenizer st;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken()); //나무의 수
+        int m = Integer.parseInt(st.nextToken()); //가져가야하는 나무의 길이
+        int max = 0;
+        int min = 0;
         tree = new int[n];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i<n; i++){
             tree[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, tree[i]); //최대값 찾기
         }
 
+        while (min < max) {
+            int mid = (max + min) / 2;
+            long sum = 0;
+            for (int treeHeight : tree) {
+                /*
+                 *  tree의 잘린 길이 = tree의 높이 - 자르는 위치(mid)
+                 *  tree의 잘린 길의의 합을 구한다.
+                 *  이 때 자르는 위치가 주어진 나무의 높이보다 클 수 있기 때문에
+                 *  0 이하인 경우(=음수)에는 합산을 하지 않고 양수만 합산하도록 해야한다.
+                 */
 
-
+                if (treeHeight - mid > 0) {
+                    sum += (treeHeight - mid);
+                }
+            }
+            if (sum < m) { // M이 길면 현재 mid가 높다는 말 mid를 낮춰야함
+                max = mid ;
+            }
+            else {
+                min = mid + 1; //M이 작다는건 너무 많이 잘랐다는 뜻 min을 높임
+            }
+        }
+        System.out.println(min - 1);
     }
 }
--- src/baekjoon/dp/계단오르기_dp.java
+++ src/baekjoon/dp/계단오르기_dp.java
@@ -8,29 +8,26 @@ import java.util.StringTokenizer;
 public class 계단오르기_dp {
     private static int[] stairs = new int[301];
     private static int[] dp = new int[301];
-    private static int result = 0;
     private static int num;
 
-    public static int solution(int[] arr, int index) {
-
-        for (int i = index; i<=num; i++) {
-
-        }
-        return 1;
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         num = Integer.parseInt(br.readLine());
         for (int i=1; i<= num; i++) {
             stairs[i] = Integer.parseInt(br.readLine());
         }
-        int cnt = 0;
-        for (int i=0; i<=num; i++) {
-            if (cnt == 0) {
-                dp[i] = Math.max(dp[i-1], dp[i-2]) + stairs[i];
+        dp[1] = stairs[1];
+        for (int i = 2; i <= num; i++) {
+            if(i == 2) {
+                dp[2] = stairs[1] + stairs[2];
             }
-
+            else if(i == 3) {
+                dp[3] = Math.max(stairs[1], stairs[2]) + stairs[3]; // 초기값 설정
+            }
+            else {
+                dp[i] = Math.max(dp[i - 3]+ stairs[i - 1], dp[i-2]) + stairs[i]; // i-3+i-1과 i-2중에 큰 값을 선택해서 더하는것
+            }
         }
-
+        System.out.println(dp[num]);
     }
 }
