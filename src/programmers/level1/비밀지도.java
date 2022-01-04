ee3f5420ae5dbcc68ad97c6fa454d23113b251e6
MyoungHyeYeon <hyeyeon2964@hanmail.net>
2022-01-04 17:44:27 +0900

    프로그래머스 비밀지도 다시 풀기

--- src/programmers/level1/비밀지도.java
+++ src/programmers/level1/비밀지도.java
@@ -27,15 +27,40 @@ public class 비밀지도 {
         }
         return answer;
     }
+    public static String[] solution1(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        StringBuilder sb = new StringBuilder();
+        int binary;
+        for (int i = 0; i<n; i++) {
+            binary = (arr1[i] | arr2[i]);
+            sb = new StringBuilder();
+            while(sb.length() != n) {
+                if (binary == 0) { // 만약 0 | 1 이였을때 자리값을 공백으로 채워야 해서 필요한 if문
+                    sb.append(" ");
+                }
+                else if (binary%2 == 1) {
+                    sb.append("#");
+                }
+                else {
+                    sb.append(" ");
+                }
+                binary = binary/2;
+            }
+            answer[i] = sb.reverse().toString();
+        }
+
+
+        return answer;
+    }
+
 
     public static void main(String[] args) {
-        int n = 5;
-        int[] arr1 = {9, 20, 28, 18, 11};
-        int[] arr2 = {30, 1, 21, 17, 28};
+        int n = 6;
+        int[] arr1 = {46, 33, 33 ,22, 31, 50};
+        int[] arr2 = {27 ,56, 19, 14, 14, 10};
 
-        System.out.println(solution(n, arr1, arr2).toString());
 
-        String[] arr = solution(n, arr1, arr2);
+        String[] arr = solution1(n, arr1, arr2);
         for(String s : arr){
             System.out.println(s);
         }
