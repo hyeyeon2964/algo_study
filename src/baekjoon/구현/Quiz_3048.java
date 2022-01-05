c63e59477c2ff835a2d2c9c0086df6e3099b8032
hyeyeon2964 <hyeyeon2964@hanmail.net>
2022-01-05 23:40:09 +0900

    백준 3048 개미 주석 추가

--- src/baekjoon/구현/Quiz_3048.java
+++ src/baekjoon/구현/Quiz_3048.java
@@ -21,8 +21,10 @@ public class Quiz_3048 {
         ant1 = sb1.reverse().toString();
         result.append(ant1);
         result.append(ant2);
+
+
         int[] arr = new int[result.length()];
-        for (int i = 0; i<arr.length; i++) {
+        for (int i = 0; i<arr.length; i++) { //그룹 비교를 위한 배열 생성 앞에 그룹은 0 뒤에 그룹은 1
             if (i<ant1.length()) {
                 arr[i] = 0;
             }
@@ -31,13 +33,13 @@ public class Quiz_3048 {
         }
         for (int i = 0; i<T; i++) {
             for (int j = 0; j<result.length()-1; j++) {
-                if(arr[j] == 0 && arr[j+1] == 1) {
+                if(arr[j] == 0 && arr[j+1] == 1) { // 만약 서로 다른 방향의 개미가 만난다면
                     arr[j] = 1;
-                    arr[j+1] = 0;
+                    arr[j+1] = 0; // 두 개미의 위치를 바꿈
                     char a = result.charAt(j);
                     char b = result.charAt(j+1);
                     result.setCharAt(j, b);
-                    result.setCharAt(j+1, a);
+                    result.setCharAt(j+1, a); // 문자열 치환
                     j++;
                 }
             }
