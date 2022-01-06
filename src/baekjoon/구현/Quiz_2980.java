35664d130583714a59fa002ab1bdcfb643480513
MyoungHyeYeon <hyeyeon2964@hanmail.net>
2022-01-06 16:24:00 +0900

    백준 도로와 신호등 입력 코드만 완성

---
+++ src/baekjoon/구현/Quiz_2980.java
@@ -0,0 +1,43 @@
+package baekjoon.구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Quiz_2980 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int N = Integer.parseInt(st.nextToken());
+//        int L = Integer.parseInt(st.nextToken());
+//        int result = 0;
+//        ArrayList<int[]> list = new ArrayList<>();
+//        for(int i = 0; i<N; i++) {
+//            int[] arr = new int[3];
+//            st = new StringTokenizer(br.readLine());
+//            arr[0] = Integer.parseInt(st.nextToken());
+//            arr[1] = Integer.parseInt(st.nextToken());
+//            arr[2] = Integer.parseInt(st.nextToken());
+//
+//
+//        }
+//    }
+public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int L = Integer.parseInt(st.nextToken());
+    int result = 0;
+    ArrayList<int[]> list = new ArrayList<>();
+    for(int i = 0; i<N; i++) {
+        int[] arr = new int[3];
+        st = new StringTokenizer(br.readLine());
+        int D = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        int G = Integer.parseInt(st.nextToken());
+
+    }
+}
+}
