ff22969f8db7b7ea566dc0903704ca1c2765f8a0
MyoungHyeYeon <hyeyeon2964@hanmail.net>
2022-01-07 21:52:04 +0900

    [해쉬] 학급회장

---
+++ src/algo_class/hash/학급회장.java
@@ -0,0 +1,32 @@
+package algo_class.hash;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class 학급회장 {
+    public static void solution(int n, String str) {
+        String[] vote = str.split("");
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i<n; i++) {
+            map.put(vote[i], map.getOrDefault(vote[i], 0)+1);
+        }
+        List<Map.Entry<String, Integer>> list = new LinkedList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+        System.out.println(list.get(0).getKey());
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        solution(n, str);
+
+
+    }
+}
--- src/baekjoon/구현/Quiz_2980.java
+++ src/baekjoon/구현/Quiz_2980.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Quiz_2980 {
@@ -37,6 +39,7 @@ public static void main(String[] args) throws IOException {
         int D = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
         int G = Integer.parseInt(st.nextToken());
+
 
     }
 }
